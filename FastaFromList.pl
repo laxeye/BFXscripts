f0b52e2f65ee7342757e7de2cb3e4e9d12b6e3e3
laxeye <oscypek@ya.ru>
2016-07-29 19:39:11 +0300

    Update FastaFromList.pl

--- FastaFromList.pl
+++ FastaFromList.pl
@@ -1,28 +1,40 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 
-open FILE,$ARGV[0] or die $!; # List of proteins, each in a new line
+open FILE,$ARGV[0] or die $!; # List of proteins/genes/contigs, each in a new line
 	my @query = <FILE>;
 close FILE;
+
+my %hash;
+
+foreach(@query){
+	chomp;
+	$hash{$_} = 1;
+}
 
 open FILE,$ARGV[1] or die $!; # Multifasta file
 	my @orig = <FILE>;
 close FILE;
+my $outfile = $ARGV[0] . "\.fna";
+open FILE,">$outfile" or die $!; # Output
 
-open FILE,'>output.fa' or die $!; # Output file
+my $printnext=0;
 
-foreach my $prot (@query){
-	chomp $prot;
-	my $printnext=0;
-	foreach (@orig){
-		if(/$prot$/){
-			print FILE;
+foreach (@orig){
+	if (/\>/) {
+		chomp;
+		$_ =~ s/\>//;
+		if(exists($hash{$_})){
+			print FILE ">$_\n";
 			$printnext=1;
 			next;
+		}else{
+			$printnext=0;
 		}
-		last if /\>/ and $printnext == 1;
-		print FILE if $printnext == 1;
 	}
+	next if $printnext==0;
+	print FILE if $printnext == 1;
 }
 
 close FILE;

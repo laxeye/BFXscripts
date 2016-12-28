e0f42b13ba41d15db6a1f7ff676b89941cf4cd51
Aleksei Korzhenkov <oscypek@ya.ru>
2016-12-28 14:29:05 +0200

    modified:   tetramers.pl 	Slight changes

--- tetramers.pl
+++ tetramers.pl
@@ -12,26 +12,25 @@ close F;
 
 shift @data;
 
-my $title;
-my $seq;
-my $length;
+my ($title, $seq, $length);
 my @nucl = ("A", "C", "G", "T");
-my @bimers;
+my @tetramers;
 my @result;
 my @names;
-my %hashtable;
 
 foreach my $oldest (@nucl){
-foreach my $older (@nucl){
-	foreach my $old (@nucl){
-		foreach(@nucl){
-			push @bimers,$oldest.$older.$old.$_;
+	foreach my $older (@nucl){
+		foreach my $old (@nucl){
+			foreach(@nucl){
+				push @tetramers,$oldest.$older.$old.$_;
+			}
 		}
 	}
 }
-}
 
-foreach(@bimers){
+print "#SequenceName";
+
+foreach(@tetramers){
 	print "\t$_";
 }
 print "\n";
@@ -42,13 +41,11 @@ foreach(@data) {
 	push @names,$title;
 	$seq =~ s/\n//;
 	$length = length($seq) + 1;
-	foreach(@bimers){
+	foreach(@tetramers){
 		push @result,sprintf "%.4f", 100*($seq =~ s/$_//gi)/$length;
 	}
-#	$C = sprintf "%.3f", $seq =~ tr/C///$length;
-#	$G = sprintf "%.3f", $seq =~ tr/G///$length;
-#	$GC = $G + $C;
 	print "$title";
+	shift @result;
 	foreach(@result){
 		print "\t$_";
 	}
@@ -58,11 +55,11 @@ foreach(@data) {
 
 sub usage{
 	print "\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\n";
-	print "\#\#\#\#\#\#\#Tetranucleotide statistics for (meta)genomic DNA\#\#\#\#\#\#\n";
-	print "\#\#\#\#\#\#\#Usage: tetramers.pl <input.fasta> > outut.csv\#\#\#\#\#\#\#\#\#\n";
-	print "\#\#\#The output is tab-separated and the 2nd column is empty\#\#\#\n";
-	print "\#\#\#\#\#\#\#\#\#\#\#\#Licensed under The MIT License (MIT)\#\#\#\#\#\#\#\#\#\#\#\#\#\n";
-	print "\#\#\#\#\#\#\#\#\#Copyright (c) Aleksei A. Korzhenkov, 2016\#\#\#\#\#\#\#\#\#\#\#\n";
+	print "\#Tetranucleotide statistics for (meta)genomic DNA sequences \#\n";
+	print "\#\#\#\#\#\# Usage: tetramers.pl <input.fasta> > outut.csv \#\#\#\#\#\#\#\#\n";
+	print "\#\#\#\#\# The output is tab-separated and includes header \#\#\#\#\#\#\#\n";
+	print "\#\#\#\#\#\#\#\#\#\#\# Licensed under The MIT License (MIT) \#\#\#\#\#\#\#\#\#\#\#\#\n";
+	print "\#\#\#\#\#\#\#\# Copyright (c) Aleksei A. Korzhenkov, 2016 \#\#\#\#\#\#\#\#\#\#\n";
 	print "\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\n";
 	exit;
 }
